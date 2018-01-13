8e2a4047da7d3b0cd326682be6bf89d78f9f8569
Yan Zhang <yan.zhang9202@gmail.com>
2018-01-13 16:36:46 -0500

    makeplot files

---
+++ Results/makeplot_mpc.m
@@ -0,0 +1,73 @@
+fig = 1;
+% Process the result data
+fstar = data.sol.fval;
+
+H = double(data.H);
+fval_avg = 1/2*diag(var.x_avg'*H*var.x_avg)/P1; % Scaled back to the actual optimality
+fval_iter = 1/2*diag(var.x'*H*var.x)/P1;
+clear H
+
+Aeq = double(data.Aeq);
+beq = double(data.beq);
+vec = Aeq*var.x_avg - repmat(beq,1,size(var.x_avg,2));
+feas_avg = diag(vec'*vec);
+vec = Aeq*var.x - repmat(beq,1,size(var.x,2));
+feas_iter = diag(vec'*vec);
+clear Aeq beq vec
+
+E = alpha*ALMparam.epsilon;
+iter = linspace(1,Ko,Ko);        
+bd_upper = (C(1)./iter+E)/P1;   % Scaled back to the normal bound
+bd_lower = -(C(2)./iter+E)/P1;
+bd_feas = (C(3)./iter+E)/P1;
+% Plot for the primal optimality
+lw = 1;
+figure(fig)
+hold on;
+h(1)=plot(fval_avg-fstar, 'r-', 'LineWidth', lw);     % Of ergodic average
+h(2)=plot(fval_iter-fstar, 'm-', 'LineWidth', lw);    % Of direct iterate
+h(3)=plot(bd_upper, 'b-', 'LineWidth', lw);   % Upper bound
+h(4)=plot(bd_lower, 'g-', 'LineWidth', lw);   % Lower bound
+legend(h, 'Ergodic average', 'Iterate', 'Upper Bound', 'Lower Bound');
+hold off;
+box on;
+% set(gca, 'Yscale', 'log')
+set(gca, 'FontSize', 15);
+% create a new pair of axes inside current figure
+axes('position',[.35 .175 .45 .3])
+box on % put box around new pair of axes
+indexOfInterest = [(Ko-10) : Ko]; % range of t near perturbation
+hold on;
+plot(indexOfInterest, fval_iter(indexOfInterest) - fstar, 'm-'); % plot on new axes
+plot(indexOfInterest, fval_avg(indexOfInterest) - fstar, 'r-'); % plot on new axes
+plot(indexOfInterest, bd_upper(indexOfInterest), 'b-'); % plot on new axes
+plot(indexOfInterest, bd_lower(indexOfInterest), 'g-'); % plot on new axes
+% set(gca, 'Yscale', 'log')
+set(gca, 'FontSize', 15)
+axis tight
+hold off;
+fig = fig + 1;
+h = [];
+% Plot for the primal feasiblity  
+figure(fig)
+hold on;
+h(1) = plot(feas_avg, 'r-', 'LineWidth', lw);
+h(2) = plot(feas_iter, 'm-', 'LineWidth', lw);
+h(3) = plot(bd_feas, 'b-', 'LineWidth', lw);
+legend(h, 'Ergodic average', 'Iterate', 'Upper Bound');
+hold off;
+box on;
+set(gca, 'Yscale', 'log')
+set(gca, 'FontSize', 15)
+% create a new pair of axes inside current figure
+axes('position',[.35 .35 .45 .3])
+box on % put box around new pair of axes
+indexOfInterest = [(Ko-10) : Ko]; % range of t near perturbation
+hold on;
+plot(indexOfInterest, feas_iter(indexOfInterest), 'm-'); % plot on new axes
+plot(indexOfInterest, feas_avg(indexOfInterest), 'r-'); % plot on new axes
+plot(indexOfInterest, bd_feas(indexOfInterest), 'b-'); % plot on new axes
+xlim([indexOfInterest(1),indexOfInterest(end)])
+set(gca, 'Yscale', 'log')
+set(gca, 'FontSize', 15)
+hold off;      
---
+++ Results/makeplot_wf.m
@@ -0,0 +1,66 @@
+%         if ~isempty(find(GPMflag==2))
+%             fprintf('Warning: There are GPM steps not successfully solved!\n')
+%         end
+%         if ALMparam.exitflag == 1
+%             fprintf(['Info: ALM succeeds after ', num2str(k), ' iterations!\n'])
+%         end
+fig = 1;
+% Process the result data
+a = double(data.a);
+fstar = sum(-log(1./(a + data.sol)));
+fval_avg = sum(-log(1./(repmat(a,1,Ko) + var.x_avg)),1);
+fval_iter = sum(-log(1./(repmat(a,1,Ko) + double(var_fp.x))),1);
+feas_avg = ones(1,data.N)*var.x_avg - double(data.pw);
+feas_iter = ones(1,data.N)*double(var_fp.x) - double(data.pw);
+E = alpha*ALMparam.epsilon;
+iter = linspace(1,Ko,Ko);        
+bd_upper = C(4)./iter+E;
+bd_lower = -(C(5)./iter+E);
+bd_feas = C(6)./iter+E;
+% Plot for the primal optimality
+lw = 1;
+figure(fig)
+hold on;
+h(1)=plot(fval_avg-fstar, 'r-', 'LineWidth', lw);     % Of ergodic average
+h(2)=plot(fval_iter-fstar, 'm-', 'LineWidth', lw);    % Of direct iterate
+h(3)=plot(bd_upper, 'b-', 'LineWidth', lw);   % Upper bound
+h(4)=plot(bd_lower, 'g-', 'LineWidth', lw);   % Lower bound
+legend(h, 'Ergodic average', 'Iterate', 'Upper Bound', 'Lower Bound');
+hold off;
+box on;
+set(gca, 'FontSize', 15);
+% create a new pair of axes inside current figure
+axes('position',[.35 .175 .45 .3])
+box on % put box around new pair of axes
+indexOfInterest = [(Ko-100) : Ko]; % range of t near perturbation
+hold on;
+plot(indexOfInterest, fval_iter(indexOfInterest) - fstar, 'm-'); % plot on new axes
+plot(indexOfInterest, fval_avg(indexOfInterest) - fstar, 'r-'); % plot on new axes
+plot(indexOfInterest, bd_upper(indexOfInterest), 'b-'); % plot on new axes
+plot(indexOfInterest, bd_lower(indexOfInterest), 'g-'); % plot on new axes
+set(gca, 'FontSize', 15)
+axis tight
+hold off;
+fig = fig + 1;
+h = [];
+% Plot for the primal feasiblity  
+figure(fig)
+hold on;
+h(1) = plot(feas_avg, 'r-', 'LineWidth', lw);
+h(2) = plot(feas_iter, 'm-', 'LineWidth', lw);
+h(3) = plot(bd_feas, 'b-', 'LineWidth', lw);
+legend(h, 'Ergodic average', 'Iterate', 'Upper Bound');
+hold off;
+box on;
+set(gca, 'FontSize', 15)
+% create a new pair of axes inside current figure
+axes('position',[.35 .35 .45 .3])
+box on % put box around new pair of axes
+indexOfInterest = [(Ko-100) : Ko]; % range of t near perturbation
+hold on;
+plot(indexOfInterest, feas_iter(indexOfInterest), 'm-'); % plot on new axes
+plot(indexOfInterest, feas_avg(indexOfInterest), 'r-'); % plot on new axes
+plot(indexOfInterest, bd_feas(indexOfInterest), 'b-'); % plot on new axes
+xlim([indexOfInterest(1),indexOfInterest(end)])
+set(gca, 'FontSize', 15)
+hold off;      
